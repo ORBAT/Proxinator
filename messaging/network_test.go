eb22d6c6a8a402b5dae1c5d2705934a41b0c2d20
Tom Eklöf <tome@unity3d.com>
2014-11-11 12:52:19 +0200

    add mock-based writing tests, "disable" example test case, random fixes

--- messaging/network_test.go
+++ messaging/network_test.go
@@ -3,9 +3,12 @@ package messaging
 import (
 	"fmt"
 	"net"
+	"os"
 	"reflect"
 	"strconv"
 	"sync"
+
+	"github.com/ORBAT/proxinator/logging"
 
 	"github.com/bradfitz/iter"
 
@@ -37,12 +40,12 @@ func (mc *mockCluster) NewMessage(purpose byte, nid wendy.NodeID, val []byte) we
 
 func (mc *mockCluster) Stop() {}
 
-func newMockConf() *Config {
-	return &Config{Address: Address(randomWendyID())}
+func newMockConf(a Address) *Config {
+	return &Config{Address: a}
 }
 
 func newMockNode(a Address) (*Node, <-chan *wendy.Message) {
-	conf := newMockConf()
+	conf := newMockConf(a)
 	nd := conf.initNode()
 
 	sentCh := make(chan *wendy.Message, 20)
@@ -60,7 +63,80 @@ func ok(v interface{}) {
 	}
 }
 
-func TestAddressMarshaling(t *testing.T) {
+func TestWriteTo(t *testing.T) {
+	from := Address(randomWendyID())
+	to := Address(randomWendyID())
+
+	nd, sentCh := newMockNode(from)
+	bytes := []byte{1, 75, 31, 44, 8, 1, 100, 20}
+
+	t.Logf("From %s\nTo %s\n", from, to)
+
+	n, err := nd.WriteTo(bytes, to)
+	if err != nil {
+		t.Fatal("WriteTo error:", err)
+	}
+	if n == 0 {
+		t.Error("WriteTo gave n = 0?")
+	}
+
+	select {
+	case sent := <-sentCh:
+		recvmsg := new(Message)
+		err := recvmsg.Decode(sent.Value)
+		ok(err)
+		t.Logf("recv\n%#v", recvmsg)
+		if recvmsg.From != from {
+			t.Errorf("Wanted From %s, got %s", from, recvmsg.From)
+		}
+
+		if recvmsg.To != to {
+			t.Errorf("Wanted To %s, got %s", to, recvmsg.To)
+		}
+	case <-time.After(50 * time.Millisecond):
+		t.Fatal("Message wasn't sent?")
+	}
+}
+
+func TestWriteToI2aS(t *testing.T) {
+	from := Address(randomWendyID())
+	to := Address(randomWendyID())
+
+	nd, sentCh := newMockNode(from)
+	msg := &Message{Version: ProtocolVersion, Data: []byte{1, 1, 2, 3, 5, 8}}
+
+	t.Logf("From %s\nTo %s\n", from, to)
+
+	n, err := nd.WriteToI2aS(msg, to)
+	if err != nil {
+		t.Fatal("WriteToI2aS error:", err)
+	}
+	if n == 0 {
+		t.Error("WriteToI2aS gave n = 0?")
+	}
+	select {
+	case sent := <-sentCh:
+		recvmsg := new(Message)
+		err := recvmsg.Decode(sent.Value)
+		ok(err)
+		t.Logf("recv\n%#v", recvmsg)
+		if recvmsg.From != from {
+			t.Errorf("Wanted From %s, got %s", from, recvmsg.From)
+		}
+
+		if recvmsg.To != to {
+			t.Errorf("Wanted To %s, got %s", to, recvmsg.To)
+		}
+
+		if !reflect.DeepEqual(recvmsg.Data, msg.Data) {
+			t.Errorf("Wanted Data %#v, got %#v", recvmsg.Data, msg.Data)
+		}
+	case <-time.After(50 * time.Millisecond):
+		t.Fatal("Message wasn't sent?")
+	}
+}
+
+func TestAddressEncoding(t *testing.T) {
 	orig := Address(randomWendyID())
 	bytes, err := orig.MarshalBinary()
 	if err != nil {
@@ -71,7 +147,7 @@ func TestAddressMarshaling(t *testing.T) {
 	err = unm.UnmarshalBinary(bytes)
 	t.Logf("bytes %#v, unm %#v", bytes, unm)
 	if err != nil {
-		t.Fatal("Couldn't unmarshal message:", err)
+		t.Fatal("Couldn't unmarshal address:", err)
 	}
 	if !reflect.DeepEqual(orig, *unm) {
 		t.Errorf("orig %#v != %#v", orig, *unm)
@@ -124,6 +200,7 @@ func TestMsgFromWendy(t *testing.T) {
 }
 
 func TestAppDeliver(t *testing.T) {
+	defer logging.LogTo(os.Stderr)()
 	numMsgs := 10
 
 	from, err := NewAddress([]byte("1111111111111111"))
@@ -201,5 +278,5 @@ func ExampleNodeReadFromI2aS() {
 
 	msg := <-got // wait for response
 	fmt.Printf("%s -> %s: %s\n", msg.From, msg.To, string(msg.Data))
-	// Output: 32323232323232323232323232323232 -> 31313131313131313131313131313131: hur de dur
+	// Outputs: 32323232323232323232323232323232 -> 31313131313131313131313131313131: hur de dur
 }
